7f9050e3c16299f9f86ce3a473030c1ae49dcded
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2020-11-30 18:02:05 +0000

    Unit tests added

---
+++ client/src/test/java/is/codion/framework/demos/chinook/model/TrackTableModelTest.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2004 - 2020, Björn Darri Sigurðsson. All Rights Reserved.
+ */
+package is.codion.framework.demos.chinook.model;
+
+import is.codion.common.db.database.Databases;
+import is.codion.common.db.exception.DatabaseException;
+import is.codion.common.model.table.ColumnConditionModel;
+import is.codion.common.user.Users;
+import is.codion.framework.db.EntityConnectionProvider;
+import is.codion.framework.db.local.LocalEntityConnectionProvider;
+import is.codion.framework.demos.chinook.domain.api.Chinook.Album;
+import is.codion.framework.demos.chinook.domain.api.Chinook.Track;
+import is.codion.framework.domain.entity.Entity;
+
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public final class TrackTableModelTest {
+
+  @Test
+  public void raisePriceOfSelected() throws DatabaseException {
+    final EntityConnectionProvider connectionProvider = createConnectionProvider();
+
+    final Entity masterOfPuppets = connectionProvider.getConnection()
+            .selectSingle(Album.TITLE, "Master Of Puppets");
+
+    final TrackTableModel trackTableModel = new TrackTableModel(connectionProvider);
+    final ColumnConditionModel<?, ?, Entity> albumConditionModel =
+            trackTableModel.getTableConditionModel().getConditionModel(Track.ALBUM_FK);
+
+    albumConditionModel.setEqualValue(masterOfPuppets);
+
+    trackTableModel.refresh();
+    assertEquals(8, trackTableModel.getRowCount());
+
+    trackTableModel.getSelectionModel().selectAll();
+    trackTableModel.raisePriceOfSelected(BigDecimal.ONE);
+
+    trackTableModel.getItems().forEach(track ->
+            assertEquals(BigDecimal.valueOf(1.99), track.get(Track.UNITPRICE)));
+  }
+
+  private EntityConnectionProvider createConnectionProvider() {
+    return new LocalEntityConnectionProvider(Databases.getInstance())
+            .setDomainClassName("is.codion.framework.demos.chinook.domain.ChinookImpl")
+            .setUser(Users.parseUser("scott:tiger"));
+  }
+}
---
+++ domain/src/test/java/is/codion/framework/demos/chinook/domain/ChinookTest.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright (c) 2004 - 2020, Björn Darri Sigurðsson. All Rights Reserved.
+ */
+package is.codion.framework.demos.chinook.domain;
+
+import is.codion.framework.domain.entity.test.EntityTestUnit;
+
+import org.junit.jupiter.api.Test;
+
+import static is.codion.framework.demos.chinook.domain.api.Chinook.*;
+
+public class ChinookTest extends EntityTestUnit {
+
+  public ChinookTest() {
+    super(ChinookImpl.class.getName());
+  }
+
+  @Test
+  public void album() throws Exception {
+    test(Album.TYPE);
+  }
+
+  @Test
+  public void artist() throws Exception {
+    test(Artist.TYPE);
+  }
+
+  @Test
+  public void customer() throws Exception {
+    test(Customer.TYPE);
+  }
+
+  @Test
+  public void employee() throws Exception {
+    test(Employee.TYPE);
+  }
+
+  @Test
+  public void genre() throws Exception {
+    test(Genre.TYPE);
+  }
+
+  @Test
+  public void invoce() throws Exception {
+    test(Invoice.TYPE);
+  }
+
+  @Test
+  public void invoiceLine() throws Exception {
+    test(InvoiceLine.TYPE);
+  }
+
+  @Test
+  public void mediaType() throws Exception {
+    test(MediaType.TYPE);
+  }
+
+  @Test
+  public void playlist() throws Exception {
+    test(Playlist.TYPE);
+  }
+
+  @Test
+  public void playlistTrack() throws Exception {
+    test(PlaylistTrack.TYPE);
+  }
+
+  @Test
+  public void track() throws Exception {
+    test(Track.TYPE);
+  }
+}
