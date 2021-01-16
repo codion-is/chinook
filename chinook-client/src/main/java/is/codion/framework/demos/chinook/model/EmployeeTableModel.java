cccea649a6c1fdd048fc8d9c79d405a6f814e8b3
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2021-01-16 12:49:31 +0000

    Codion upgrade

---
+++ chinook-client/src/main/java/is/codion/framework/demos/chinook/model/EmployeeTableModel.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2004 - 2021, Björn Darri Sigurðsson. All Rights Reserved.
+ */
+package is.codion.framework.demos.chinook.model;
+
+import is.codion.framework.db.EntityConnectionProvider;
+import is.codion.swing.framework.model.SwingEntityTableModel;
+import is.codion.swing.framework.model.SwingEntityTreeModel;
+
+import static is.codion.framework.demos.chinook.domain.api.Chinook.Employee;
+
+public final class EmployeeTableModel extends SwingEntityTableModel {
+
+  private final SwingEntityTreeModel treeModel;
+
+  public EmployeeTableModel(final EntityConnectionProvider connectionProvider) {
+    super(Employee.TYPE, connectionProvider);
+    this.treeModel = new SwingEntityTreeModel(this, Employee.REPORTSTO_FK);
+  }
+
+  public SwingEntityTreeModel getTreeModel() {
+    return treeModel;
+  }
+}
