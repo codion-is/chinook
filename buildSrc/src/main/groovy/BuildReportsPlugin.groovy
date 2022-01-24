5978e9f198bab3f95ae0aad27849a157198cf433
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2022-01-24 12:55:12 +0000

    BuildReportsExtension, get classpaths from project instead of extension

--- buildSrc/src/main/groovy/BuildReportsPlugin.groovy
+++ buildSrc/src/main/groovy/BuildReportsPlugin.groovy
@@ -1,7 +1,8 @@
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.file.FileCollection
+import org.gradle.api.plugins.JavaPluginExtension
 import org.gradle.api.provider.Property
+import org.gradle.api.tasks.SourceSet
 /**
  * Plugin for building JasperReports reports.
  */
@@ -15,10 +16,13 @@ class BuildReportsPlugin implements Plugin<Project> {
             inputs.dir config.sourceDir
             outputs.dir config.targetDir
             doLast {
-                ant.taskdef(name: 'jrc', classname: 'net.sf.jasperreports.ant.JRAntCompileTask', classpath: config.taskClasspath.get())
+                final JavaPluginExtension javaPlugin = project.getExtensions().getByType(JavaPluginExtension.class);
+                final SourceSet main = javaPlugin.getSourceSets().findByName("main");
+                ant.taskdef(name: 'jrc', classname: 'net.sf.jasperreports.ant.JRAntCompileTask',
+                        classpath: main.getRuntimeClasspath().asPath)
                 config.targetDir.get().mkdirs()
                 ant.jrc(srcdir: config.sourceDir.get(), destdir: config.targetDir.get()) {
-                    classpath(path: config.buildClasspath.get())
+                    classpath(path: main.output.getResourcesDir().toString())
                     include(name: '**/*.jrxml')
                 }
             }
@@ -34,14 +38,6 @@ class BuildReportsPlugin implements Plugin<Project> {
 }
 
 abstract class BuildReportsExtension {
-    /**
-     * @return The classpath on which to find the JRAntCompileTask.
-     */
-    abstract Property<String> getTaskClasspath()
-    /**
-     * @return The classpath to use when compiling the reports
-     */
-    abstract Property<FileCollection> getBuildClasspath()
     /**
      * @return The reports source dir
      */
