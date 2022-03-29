33542af3863199e983257d8e127a7f5be51d9c56
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2022-03-29 14:24:19 +0000

    Codion upgraded

--- chinook-client/src/main/java/is/codion/framework/demos/chinook/ui/ChinookAppPanel.java
+++ chinook-client/src/main/java/is/codion/framework/demos/chinook/ui/ChinookAppPanel.java
@@ -13,10 +13,10 @@ import is.codion.framework.db.EntityConnectionProvider;
 import is.codion.framework.demos.chinook.model.ChinookApplicationModel;
 import is.codion.framework.demos.chinook.model.EmployeeTableModel;
 import is.codion.framework.model.EntityEditModel;
-import is.codion.swing.common.ui.Utilities;
-import is.codion.swing.common.ui.combobox.Completion;
+import is.codion.swing.common.ui.component.combobox.Completion;
 import is.codion.swing.common.ui.control.Control;
 import is.codion.swing.common.ui.control.Controls;
+import is.codion.swing.common.ui.dialog.LookAndFeelSelectionDialogBuilder;
 import is.codion.swing.common.ui.laf.LookAndFeelProvider;
 import is.codion.swing.framework.model.SwingEntityModel;
 import is.codion.swing.framework.ui.EntityApplicationPanel;
@@ -197,7 +197,7 @@ public final class ChinookAppPanel extends EntityApplicationPanel<ChinookApplica
   public static void main(String[] args) throws CancelException {
     String language = UserPreferences.getUserPreference(LANGUAGE_PREFERENCES_KEY, Locale.getDefault().getLanguage());
     Locale.setDefault(LANGUAGE_IS.equals(language) ? LOCALE_IS : LOCALE_EN);
-    Utilities.CHANGE_LOOK_AND_FEEL_DURING_SELECTION.set(true);
+    LookAndFeelSelectionDialogBuilder.CHANGE_LOOK_AND_FEEL_DURING_SELECTION.set(true);
     addRadianceLookAndFeels();
     Completion.COMBO_BOX_COMPLETION_MODE.set(Completion.Mode.AUTOCOMPLETE);
     FilteredModel.ASYNC_REFRESH.set(true);
