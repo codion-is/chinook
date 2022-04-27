b2f54c97146cd52a43d53f09ed2421d4275d459b
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2022-04-27 16:31:02 +0000

    Codion upgraded

--- chinook-client/src/main/java/is/codion/framework/demos/chinook/ui/PlaylistTablePanel.java
+++ chinook-client/src/main/java/is/codion/framework/demos/chinook/ui/PlaylistTablePanel.java
@@ -61,14 +61,14 @@ public final class PlaylistTablePanel extends EntityTablePanel {
 
     @Override
     protected RandomPlaylistParameters getComponentValue(RandomPlaylistParametersPanel component) {
-      return new RandomPlaylistParameters(component.playlistNameField.getText(), component.noOfTracksField.getValue());
+      return new RandomPlaylistParameters(component.playlistNameField.getText(), component.noOfTracksField.getNumber());
     }
 
     @Override
     protected void setComponentValue(RandomPlaylistParametersPanel component,
                                      RandomPlaylistParameters parameters) {
       component.playlistNameField.setText(parameters.playlistName());
-      component.noOfTracksField.setValue(parameters.noOfTracks());
+      component.noOfTracksField.setNumber(parameters.noOfTracks());
     }
   }
 
