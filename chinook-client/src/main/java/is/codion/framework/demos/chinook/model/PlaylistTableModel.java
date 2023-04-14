a56e83f2710696d3a3d6e5ba133d57d82a78b830
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2023-04-14 11:30:37 +0000

    CreateRandomPlaylistFunction, transaction control moved to call-site, unit test added

--- chinook-client/src/main/java/is/codion/framework/demos/chinook/model/PlaylistTableModel.java
+++ chinook-client/src/main/java/is/codion/framework/demos/chinook/model/PlaylistTableModel.java
@@ -4,6 +4,7 @@
 package is.codion.framework.demos.chinook.model;
 
 import is.codion.common.db.exception.DatabaseException;
+import is.codion.framework.db.EntityConnection;
 import is.codion.framework.db.EntityConnectionProvider;
 import is.codion.framework.demos.chinook.domain.api.Chinook.Playlist;
 import is.codion.framework.demos.chinook.domain.api.Chinook.Playlist.RandomPlaylistParameters;
@@ -18,10 +19,28 @@ public final class PlaylistTableModel extends SwingEntityTableModel {
     super(Playlist.TYPE, connectionProvider);
   }
 
-  public void createRandomPlaylist(RandomPlaylistParameters parameters) throws DatabaseException {
-    Entity randomPlaylist = connectionProvider().connection()
-            .executeFunction(Playlist.RANDOM_PLAYLIST, parameters);
+   public void createRandomPlaylist(RandomPlaylistParameters parameters) throws DatabaseException {
+    Entity randomPlaylist = createPlaylist(parameters);
     addEntitiesAt(0, Collections.singletonList(randomPlaylist));
     selectionModel().setSelectedItem(randomPlaylist);
+  }
+
+  private Entity createPlaylist(RandomPlaylistParameters parameters) throws DatabaseException {
+    EntityConnection connection = connectionProvider().connection();
+    connection.beginTransaction();
+    try {
+      Entity randomPlaylist = connection.executeFunction(Playlist.RANDOM_PLAYLIST, parameters);
+      connection.commitTransaction();
+
+      return randomPlaylist;
+    }
+    catch (DatabaseException e) {
+      connection.rollbackTransaction();
+      throw e;
+    }
+    catch (Exception e) {
+      connection.rollbackTransaction();
+      throw new RuntimeException(e);
+    }
   }
 }
