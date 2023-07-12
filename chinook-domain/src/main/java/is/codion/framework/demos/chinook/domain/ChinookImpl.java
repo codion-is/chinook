de78d45a1a099416f5f12f09907ba2a5895d8730
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2023-07-12 10:35:24 +0000

    Codion upgrade

--- chinook-domain/src/main/java/is/codion/framework/demos/chinook/domain/ChinookImpl.java
+++ chinook-domain/src/main/java/is/codion/framework/demos/chinook/domain/ChinookImpl.java
@@ -20,7 +20,7 @@ package is.codion.framework.demos.chinook.domain;
 
 import is.codion.common.db.exception.DatabaseException;
 import is.codion.common.db.operation.DatabaseFunction;
-import is.codion.common.formats.LocaleDateTimePattern;
+import is.codion.common.format.LocaleDateTimePattern;
 import is.codion.framework.db.EntityConnection;
 import is.codion.framework.db.condition.SelectCondition;
 import is.codion.framework.db.local.LocalEntityConnection;
