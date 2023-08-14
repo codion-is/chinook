d4e137c3dd34ce690a74081540f11578ceea3cec
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2023-08-14 15:58:38 +0000

    Codion upgrade

--- chinook-domain/src/main/java/is/codion/framework/demos/chinook/server/ChinookLoginProxy.java
+++ chinook-domain/src/main/java/is/codion/framework/demos/chinook/server/ChinookLoginProxy.java
@@ -101,7 +101,7 @@ public final class ChinookLoginProxy implements LoginProxy {
 
   private void authenticateUser(User user) throws LoginException {
     try (EntityConnection connection = getConnectionFromPool()) {
-      int rows = connection.rowCount(and(
+      int rows = connection.count(and(
               column(Authentication.User.USERNAME)
                       .equalToIgnoreCase(user.username()),
               column(Authentication.User.PASSWORD_HASH)
