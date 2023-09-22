6c929b72d547aecce94abce17ebc80109fe0c01a
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2023-09-22 11:50:30 +0200

    Codion upgrade

--- chinook-client/src/main/java/is/codion/framework/demos/chinook/model/InvoiceModel.java
+++ chinook-client/src/main/java/is/codion/framework/demos/chinook/model/InvoiceModel.java
@@ -34,6 +34,6 @@ public final class InvoiceModel extends SwingEntityModel {
     detailModelLink.clearForeignKeyOnEmptySelection().set(true);
     detailModelLink.active().set(true);
 
-    invoiceLineEditModel.addTotalsUpdatedListener(tableModel()::replaceEntities);
+    invoiceLineEditModel.addTotalsUpdatedListener(tableModel()::replace);
   }
 }
