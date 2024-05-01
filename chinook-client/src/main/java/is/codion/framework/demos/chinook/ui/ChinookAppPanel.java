e853ef4a0a1b82d65cfb702074860635b4a9024f
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2024-05-01 21:21:36 +0200

    Codion upgrade

--- chinook-client/src/main/java/is/codion/framework/demos/chinook/ui/ChinookAppPanel.java
+++ chinook-client/src/main/java/is/codion/framework/demos/chinook/ui/ChinookAppPanel.java
@@ -177,7 +177,7 @@ public final class ChinookAppPanel extends EntityApplicationPanel<ChinookAppMode
 	public static void main(String[] args) throws CancelException {
 		String language = UserPreferences.getUserPreference(LANGUAGE_PREFERENCES_KEY, Locale.getDefault().getLanguage());
 		Locale.setDefault(LANGUAGE_IS.equals(language) ? LOCALE_IS : LOCALE_EN);
-		Arrays.stream(FlatAllIJThemes.INFOS).forEach(LookAndFeelProvider::addLookAndFeelProvider);
+		Arrays.stream(FlatAllIJThemes.INFOS).forEach(LookAndFeelProvider::addLookAndFeel);
 		Completion.COMBO_BOX_COMPLETION_MODE.set(Completion.Mode.AUTOCOMPLETE);
 		EntityPanel.Config.TOOLBAR_CONTROLS.set(true);
 		EntityPanel.Config.WINDOW_TYPE.set(WindowType.FRAME);
