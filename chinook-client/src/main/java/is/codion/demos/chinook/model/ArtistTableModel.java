94ef0996e3e59f55d571d7d747f1d6a4acf62b1b
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2025-05-19 11:01:54 +0000

    Combine artists functionality added

---
+++ chinook-client/src/main/java/is/codion/demos/chinook/model/ArtistTableModel.java
@@ -0,0 +1,56 @@
+/*
+ * This file is part of Codion Chinook Demo.
+ *
+ * Codion Chinook Demo is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Codion Chinook Demo is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Codion Chinook Demo.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Copyright (c) 2025, Björn Darri Sigurðsson.
+ */
+package is.codion.demos.chinook.model;
+
+import is.codion.demos.chinook.domain.api.Chinook.Album;
+import is.codion.demos.chinook.domain.api.Chinook.Artist;
+import is.codion.framework.db.EntityConnection;
+import is.codion.framework.db.EntityConnectionProvider;
+import is.codion.framework.domain.entity.Entity;
+import is.codion.framework.model.EntityEditModel;
+import is.codion.swing.framework.model.SwingEntityTableModel;
+
+import java.util.List;
+
+import static is.codion.framework.db.EntityConnection.Update.where;
+import static is.codion.framework.db.EntityConnection.transaction;
+import static is.codion.framework.domain.entity.Entity.primaryKeys;
+
+public final class ArtistTableModel extends SwingEntityTableModel {
+
+	public ArtistTableModel(EntityConnectionProvider connectionProvider) {
+		super(Artist.TYPE, connectionProvider);
+	}
+
+	public void combine(List<Entity> artistsToDelete, Entity artistToKeep) {
+		EntityConnection connection = connection();
+		transaction(connection, () -> {
+			connection.update(where(Album.ARTIST_FK.in(artistsToDelete))
+							.set(Album.ARTIST_ID, artistToKeep.primaryKey().value())
+							.build());
+			connection.delete(primaryKeys(artistsToDelete));
+		});
+	}
+
+	public void onCombined(List<Entity> artistsToDelete, Entity artistToKeep) {
+		selection().item().set(artistToKeep);
+		items().remove(artistsToDelete);
+		EntityEditModel.editEvents().deleted(Artist.TYPE).accept(artistsToDelete);
+	}
+}
